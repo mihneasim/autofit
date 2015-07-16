80d48f8448c5c22512a0ff7f3ac3d839fb3a0258
Mihnea Simian <contact@mesimian.com>
2015-07-16 13:55:29 +0300

    read_gif borrowed method, requires numpy which i'm not very happy of

--- src/autofit.py
+++ src/autofit.py
@@ -1,13 +1,18 @@
 """ Simple geometry for resizing and cropping """
 from fractions import gcd
+import os
 
-from PIL import Image
+import numpy
+import PIL
 
 
-def max_bounds(src_w, src_h, ratio_w, ratio_h):
+def max_bounds(dest_w, dest_h, ratio_w, ratio_h):
 	"""
-	Given an area src_w x src_h, determine the maximum area
-	of ratio ratio_w:ratio_h which we can fit inside the original area
+	Given an area dest_w x dest_h, determine the 2 points (top left,
+	bottom right) of the rectangle
+	that fits in. Rectangle keeps ratio_w:ratio_h
+
+	returns (x0, y0, x1, y1)
 
 	"""
 	raise NotImplemented
@@ -39,7 +44,7 @@ def center_crop(src_w, src_h, dest_w, dest_h):
 	"""
 	Return the 4-tuple box required for center cropping to dest_w, dest_h
 
-	This method requires pre-prepared sizes with max_bouds
+	This method requires pre-prepared sizes with resize_for
 
 	"""
 	if src_w == dest_w:
@@ -53,7 +58,7 @@ def center_crop(src_w, src_h, dest_w, dest_h):
 		raise ValueError("Either height or width should match. Use resize_for")
 
 
-def autofit(image, dest_w, dest_h):
+def autofit_crop(image, dest_w, dest_h):
 	"""
 	Resize and center crop image to dest_w, dest_h
 
@@ -62,7 +67,21 @@ def autofit(image, dest_w, dest_h):
 	"""
 	resize_w, resize_h = resize_for(image.size[0], image.size[1],
 			dest_w, dest_h)
-	out = image.resize((resize_w, resize_h), Image.ANTIALIAS)
+	out = image.resize((resize_w, resize_h), PIL.Image.ANTIALIAS)
+	return out.crop(center_crop(resize_w, resize_h, dest_w, dest_h))
+
+
+def autofit_fill(image, dest_w, dest_h, fill=None):
+	"""
+	Resize and center image to max bounds
+	If ratios are different, fill with `fill` (default None, transparency)
+
+	image is PIL.Image object, so is returned value
+
+	"""
+	resize_w, resize_h = resize_for(image.size[0], image.size[1],
+			dest_w, dest_h)
+	out = image.resize((resize_w, resize_h), PIL.Image.ANTIALIAS)
 	return out.crop(center_crop(resize_w, resize_h, dest_w, dest_h))
 
 
@@ -78,3 +97,39 @@ def ratio2float(ratio):
 	""" Converts '16:9' to 1.7777777 """
 	w, h = ratio.split(':')
 	return float(w) / float(h)
+
+
+def read_gif(filename, as_numpy=True):
+	"""
+	Read images from an animated GIF file. Return a list of numpy
+	arrays, or, if as_numpy is false, a list if PIL images.
+		Adapted from (C) 2012, Almar Klein, Ant1, Marius van Voorden,
+		code subject to the (new) BSD license
+
+	"""
+	if not os.path.isfile(filename):
+		raise IOError('File not found: %s' % filename)
+
+	pil_im = PIL.Image.open(filename)
+	pil_im.seek(0)
+
+	# Read all images inside
+	images = []
+	try:
+		while True:
+			# Get image as numpy array
+			tmp = pil_im.convert()  # Make without palette
+			a = numpy.asarray(tmp)
+			if not a.shape:
+				raise MemoryError("Too little memory to convert PIL image to array")
+			# Store, and next
+			images.append(a)
+			pil_im.seek(pil_im.tell() + 1)
+	except EOFError:
+		pass
+
+	# Convert to normal PIL images if needed
+	if not as_numpy:
+		return [PIL.Image.fromarray(im) for im in images]
+	else:
+		return images
